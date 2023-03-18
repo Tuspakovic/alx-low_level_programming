cc018d2a0bb33eededda9f4ae899dcd2a52fd74c
root <martinoasante@yahoo.com>
2023-03-17 17:40:34 -0700

    task 8 update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
-
 /**
-   * main - prints a message to standard error
-    *
-     * Return: (1) always
-      */
+ * main - Entry point
+ * Return: Always 1
+ */
 int main(void)
 {
-		char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-			int len = 0;
-
-				while (msg[len] != '\0')
-							len++;
-
-					write(STDERR_FILENO, msg, len);
-
-						return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
-
